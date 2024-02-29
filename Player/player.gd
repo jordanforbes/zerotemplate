2f9daa1fc721a88e57e91c9dbb7a40b002ff44c7
Jordan Forbes <jordforb@gmail.com>
2024-02-29 10:48:52 -0500

    run forward animation

--- Player/player.gd
+++ Player/player.gd
@@ -5,17 +5,20 @@ extends CharacterBody2D
 @export var jump_velocity : float = -400.0
 @export var double_jump_velocity : float = -200.0
 
-var double_jump_usable : bool = true
+@onready var animated_sprite :AnimatedSprite2D = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var double_jump_usable : bool = true
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
+		#double jump resets when on ground
 		double_jump_usable = true
 
 	# Handle jump.
@@ -31,10 +34,19 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation()
+	
+func update_animation() -> void: 
+	if not animation_locked: 
+		if direction.x != 0:
+			animated_sprite.play("Run")
+		else:
+			animated_sprite.play("Idle")
+		
